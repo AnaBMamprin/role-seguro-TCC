0ebfa54252272b356de043d871260867f1a7807f
LuisCassiano1 <luiscassiano3706@gmail.com>
2025-10-29 16:18:52 -0300

    pequenas alterações no pom

--- src/main/java/com/example/app1/service/AvaliacaoService.java
+++ src/main/java/com/example/app1/service/AvaliacaoService.java
@@ -1,7 +1,7 @@
 package com.example.app1.service;
 
 import java.util.List;
-import java.util.Optional; // Importe o Optional
+import java.util.Optional; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
